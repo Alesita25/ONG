7a72c857daa0d93a3c36343b61281b7006ab5726
Alesita25 <alej.aranguiz@duocuc.cl>
2022-04-26 14:22:22 -0400

    Actividad 2

---
+++ js/valida.js
@@ -0,0 +1,15 @@
+
+
+// var validator = new FormValidator('contacto_form', [{
+//     name: 'NombreCliente',
+//     display: 'required',
+//     rules: 'required'
+// },   ], function(errors, event) {
+//     if (errors.length > 0) {
+//         // Show the errors
+//     }
+// });
+
+$(document).ready(function(){
+    alert("mensaje");
+});
